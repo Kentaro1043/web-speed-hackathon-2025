fe4c80ab797f497ab2ed594eb5424049ab57e542
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-03-23 07:30:00 +0900

    wip

--- workspaces/server/drizzle.config.ts
+++ workspaces/server/drizzle.config.ts
@@ -6,9 +6,14 @@ const require = createRequire(import.meta.url);
 
 export default defineConfig({
   dbCredentials: {
-    url: process.env['DATABASE_URL'] || '',
+    ssl: false,
+    host: 'db',
+    port: 5432,
+    user: 'user',
+    password: 'password',
+    database: 'database',
   },
-  dialect: 'turso',
+  dialect: 'postgresql',
   out: './migrations',
   schema: require.resolve('@wsh-2025/schema/src/database/schema.ts'),
 });
