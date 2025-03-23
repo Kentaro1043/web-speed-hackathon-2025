c2f367990a2e15e5621be9be50bfeeccac6e8ce9
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-03-23 12:38:02 +0900

    lodash消した

--- workspaces/client/src/app/createStore.ts
+++ workspaces/client/src/app/createStore.ts
@@ -1,5 +1,4 @@
 import { withLenses } from '@dhmk/zustand-lens';
-import _ from 'lodash';
 import { createStore as createZustandStore } from 'zustand/vanilla';
 
 import { createAuthStoreSlice } from '@wsh-2025/client/src/features/auth/stores/createAuthStoreSlice';
@@ -39,7 +38,7 @@ export const createStore = ({ hydrationData }: Props) => {
     })),
   );
 
-  store.setState((s) => _.merge(s, hydrationData));
+  store.setState((s) => Object.assign(s, hydrationData));
 
   return store;
 };
--- workspaces/client/src/pages/timetable/stores/createTimetablePageStoreSlice.ts
+++ workspaces/client/src/pages/timetable/stores/createTimetablePageStoreSlice.ts
@@ -2,10 +2,38 @@ import { lens } from '@dhmk/zustand-lens';
 import { StandardSchemaV1 } from '@standard-schema/spec';
 import * as schema from '@wsh-2025/schema/src/api/schema';
 import { produce } from 'immer';
-import _ from 'lodash';
+
 import { ArrayValues } from 'type-fest';
 
 import { DEFAULT_WIDTH } from '@wsh-2025/client/src/features/timetable/constants/grid_size';
+
+// TypeScript対応したdebounce実装
+type AnyFunction = (...args: any[]) => any;
+
+const debounce = <F extends AnyFunction>(
+  func: F, 
+  delay: number, 
+  options: { leading?: boolean } = {}
+): ((...args: Parameters<F>) => void) => {
+  let timerId: ReturnType<typeof setTimeout> | undefined;
+  let shouldInvoke: boolean;
+
+  return (...args: Parameters<F>) => {
+    shouldInvoke = true;
+
+    if (!timerId && options.leading) {
+      func(...args);
+      shouldInvoke = false;
+    }
+    
+    if (timerId) clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      if (shouldInvoke) func(...args);
+      timerId = undefined;
+    }, delay);
+  };
+};
 
 type ChannelId = string;
 type Program = ArrayValues<StandardSchemaV1.InferOutput<typeof schema.getTimetableResponse>>;
@@ -41,7 +69,7 @@ export const createTimetablePageStoreSlice = () => {
     },
     columnWidthRecord: {},
     currentUnixtimeMs: 0,
-    refreshCurrentUnixtimeMs: _.debounce(() => {
+    refreshCurrentUnixtimeMs: debounce(() => {
       set(() => ({
         currentUnixtimeMs: Date.now(),
       }));
